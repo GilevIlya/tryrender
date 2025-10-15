b837167df37a0a4e43d8b8dc2c6a5f7ca7d76feb
Ilya <gilevilia2009@gmail.com>
2025-10-15 13:28:17 +0300

    push

--- config.py
+++
@@ -1,8 +0,0 @@
-TOKEN = '8276388473:AAER8xmADLMqwmf8H4pQFfcS9SxO-haAvMQ'
-
-CONNECTION = {
-    "user": "postgres",
-    "password": "1amtheBest",
-    "database": "telegramweatherandexchangerratebot",
-    "host": "localhost"
-}
